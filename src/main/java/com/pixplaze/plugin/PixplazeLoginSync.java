d1f98bad38c68cc29061f56dd5a411a74304cd07
pazik98 <nefedow98@mail.ru>
2022-04-06 02:16:05 +0300

    Реализована поддержка конфига
    	-	Изменена версия плагина в файле `pom.xml`.
    
    	-	Реализовано копирование дефолтного конфига и файлов локализации в папку проекта.
    
    	-	Большая часть функционала класса `PixplazeLoginSync.java` в методе `OnEnable()` вынесена в разные вспомогательные методы.
    
    	-	Добавлены файлы `config.yml` и `lang/ru_RU.yml` в `resources`.

--- src/main/java/com/pixplaze/plugin/PixplazeLoginSync.java
+++ src/main/java/com/pixplaze/plugin/PixplazeLoginSync.java
@@ -7,14 +7,18 @@ import com.pixplaze.keyword.plugin.PixplazeKeywordLib;
 import com.pixplaze.sync.database.sql.ConnectionManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.LoggerContext;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.beans.ExceptionListener;
+import java.io.File;
 import java.util.Locale;
 import java.util.Objects;
 
 public final class PixplazeLoginSync extends JavaPlugin {
 
     private Dictionary dictionary;
+    private final FileConfiguration config = getConfig();
 
     private static PixplazeLoginSync instance;
 
@@ -28,28 +32,44 @@ public final class PixplazeLoginSync extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        dictionary = PixplazeKeywordLib.fromLanguageDirectory(Locale.forLanguageTag("ru-RU"), this.getDataFolder() + "/lang");
-
-        ((LoggerContext) LogManager.
-                getContext(false)).getConfiguration().
-                getLoggerConfig(LogManager.ROOT_LOGGER_NAME).
-                addFilter(new PasswordFilter());
-
+        copyDefaultFiles();
+        initDictionary();
+        setLoggerFilter();
         getServer().getPluginManager().registerEvents(new PlayerListener(), this);
-
-        Objects.requireNonNull(getCommand("login")).setExecutor(new LoginCmdExecutor());
-        Objects.requireNonNull(getCommand("logout")).setExecutor(new LogoutCmdExecutor());
-        Objects.requireNonNull(getCommand("register")).setExecutor(new RegisterCmdExecutor());
-
+        registerCommands();
         ConnectionManager.getInstance().connect();
-    }
-
-    public Dictionary getDictionary() {
-        return dictionary;
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private void copyDefaultFiles() {
+        this.saveDefaultConfig();
+        saveResource("lang/ru_RU.yml", false);
+    }
+
+    private void initDictionary() {
+        String defaultLocale = config.getString("default-locale");
+        dictionary = PixplazeKeywordLib.fromLanguageDirectory(Locale.forLanguageTag(defaultLocale),
+                this.getDataFolder() + "/lang");
+    }
+
+    private void setLoggerFilter() {
+        ((LoggerContext) LogManager.
+                getContext(false)).getConfiguration().
+                getLoggerConfig(LogManager.ROOT_LOGGER_NAME).
+                addFilter(new PasswordFilter());
+    }
+
+    private void registerCommands() {
+        Objects.requireNonNull(getCommand("login")).setExecutor(new LoginCmdExecutor());
+        Objects.requireNonNull(getCommand("logout")).setExecutor(new LogoutCmdExecutor());
+        Objects.requireNonNull(getCommand("register")).setExecutor(new RegisterCmdExecutor());
+    }
+
+    public Dictionary getDictionary() {
+        return dictionary;
     }
 }
